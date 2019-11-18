d2a14e93d660029851c87b35f80cfaefe3cad3c8
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-11-18 09:01:35 -0500

    set up the basic api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,12 @@
 const path = require('path');
 
 module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+        res.json("all possible friends")
+    })
 
+    app.post("/api/friends", function(req, res) {
+        console.log(req)
+        res.end()
+    })
 }
